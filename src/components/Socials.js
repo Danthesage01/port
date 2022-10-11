e9b1760f02504d0752e9f1defce7467813a648ef
Daniel Ayeni <ayenibabajidedaniel@gmail.com>
2022-10-11 15:23:16 +0100

    Change resume hosting link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,42 +1,73 @@
-import React from 'react'
-import { FaFilePdf, FaTwitter, FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa"
+import React from "react";
+import {
+  FaFilePdf,
+  FaTwitter,
+  FaGithub,
+  FaEnvelope,
+  FaLinkedin,
+} from "react-icons/fa";
 const Socials = () => {
   return (
     <>
       <ul className="social-icons hero-icons">
-        <li className='tooltip'>
-          <a href="https://drive.google.com/file/d/19BXK-KJ1aktLAPi_CrIMen_o6FeXeBAa/view?usp=sharing" rel='noopener noreferrer' target="_blank" className='social-icon '>
+        <li className="tooltip">
+          <a
+            href="https://drive.google.com/file/d/1qTNRf1MvQcIpBwJELTrLaPCwybnY3CiN/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="social-icon "
+          >
             <FaFilePdf />
           </a>
-          <span className='tooltiptext'>Resumé</span>
+          <span className="tooltiptext">Resumé</span>
         </li>
-        <li className='tooltip'>
-          <a href="https://github.com/Danthesage01" rel='noopener noreferrer' target="_blank" className='social-icon'>
+        <li className="tooltip">
+          <a
+            href="https://github.com/Danthesage01"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="social-icon"
+          >
             <FaGithub />
           </a>
-          <span className='tooltiptext'>Github</span>
+          <span className="tooltiptext">Github</span>
         </li>
-        <li className='tooltip'>
-          <a href="https://twitter.com/TheSageConcepts" rel='noopener noreferrer' target="_blank" className='social-icon'>
+        <li className="tooltip">
+          <a
+            href="https://twitter.com/TheSageConcepts"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="social-icon"
+          >
             <FaTwitter />
           </a>
-          <span className='tooltiptext'>Twitter</span>
+          <span className="tooltiptext">Twitter</span>
         </li>
-        <li className='tooltip'>
-          <a href="https://www.linkedin.com/in/danthesage/" rel='noopener noreferrer' target="_blank" className='social-icon'>
+        <li className="tooltip">
+          <a
+            href="https://www.linkedin.com/in/danthesage/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="social-icon"
+          >
             <FaLinkedin />
           </a>
-          <span className='tooltiptext'>LinkedIn</span>
+          <span className="tooltiptext">LinkedIn</span>
         </li>
-        <li className='tooltip'>
-          <a href="mailto:ayenibabajidedaniel@gmail.com" rel='noopener noreferrer' target="_blank" className='social-icon'>
+        <li className="tooltip">
+          <a
+            href="mailto:ayenibabajidedaniel@gmail.com"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="social-icon"
+          >
             <FaEnvelope />
           </a>
-          <span className='tooltiptext'>Mail</span>
+          <span className="tooltiptext">Mail</span>
         </li>
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
