cb5494c71106999c09daf04547e62a99a57d5899
Daniel Ayeni <ayenibabajidedaniel@gmail.com>
2022-10-25 23:27:41 +0100

    airbnb clone  project added

--- src/data.js
+++ src/data.js
@@ -2,13 +2,13 @@ const data = [
   {
     id: 1,
     category: "REACT",
-    image: "/images/quiz-main.jpg",
-    title: "Quiz App",
-    description: `I developed the Quiz App to showcase my knowledge and understanding of ReactJS in developing
-             interactive and responsive single-page applications. I used create react-app in building the App alongside several React Hooks such as the Context API, useEffect, and useState. I consumed a REST API that returns an array of random questions and answers which I massaged to create the quiz app. I got the project idea from one of Scrimba's solo projects.`,
-    github: "https://github.com/Danthesage01/quizzical",
-    url: "https://r-quizzical.netlify.app/",
-    technology: "HTML5, CSS3, ReactJS & Context API"
+    image: "/images/airbnb.jpg",
+    title: "Airbnb Clone",
+    description: `I am currently working on this Airbnb Clone to showcase my knowledge and understanding of React.js in developing
+            modern, responsive and user-friendly applications. I used create react-app and utilised react-router-dom for easy navigations and routing within the app. I am currently using mock data as database on the application, however, I intend to integrate Firebase 9 for the database and authentication.`,
+    github: "https://github.com/Danthesage01/airbnb",
+    url: "https://zts-airbnb.netlify.app/",
+    technology: "HTML5, CSS-in-JS, ReactJS & React-Router-DOM",
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ const data = [
     description: `I built the Shoe Ecommerce store to showcase my knowledge and understanding of modern JavaScript (ES6+) which includes AJAX, DOM & Module and internet research skills in creating useful software. Firstly, I created and hosted a Fake REST API server on Heroku that serves as the backend. Then, I fetched the data from the server and massaged them to produce a responsive and interactive ecommerce store. The app features AddToCart, DOM rendering, filters, single-page rendering, and top scroll navigation.`,
     github: "https://github.com/Danthesage01/ecommerce-shoe-vendor",
     url: "https://ecommerce-sv.netlify.app",
-    technology: "HTML5, CSS3, JavaScript & ES6+ Modules"
+    technology: "HTML5, CSS3, JavaScript & ES6+ Modules",
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const data = [
     description: `I developed the Sneakers Ecommerce Website to showcase my knowledge and understanding of JavaScript, ReactJS, and the integration of other third-party technologies such as Netlify Serverless Functions, Auth0 and Stripe to build a modern, responsive and user-friendly ecommerce application for 21st century business outfits. The ecommerce app features a Payment Gateway, User Authentication and other essential ecommerce features.`,
     github: "https://github.com/Danthesage01/sneakers",
     url: "https://re-sneakers.netlify.app",
-    technology: "ReactJS, Styled Components(CSS), Auth0 & Stripe, JavaScript"
+    technology: "ReactJS, Styled Components(CSS), Auth0 & Stripe, JavaScript",
   },
   {
     id: 4,
@@ -39,17 +39,18 @@ const data = [
      CSS3 to develop modern, responsive and user-friendly websites, landing pages and web apps. The wakatours website was designed mainly with CSS Flexbox. Also, I integrated some JavaScript concepts and functions to ensure the website fully dynamically and interactive.`,
     github: "https://github.com/Danthesage01/wakatours-webites",
     url: "https://wakatours.netlify.app",
-    technology: "HTML5, CSS3, & JavaScript"
+    technology: "HTML5, CSS3, & JavaScript",
   },
   {
     id: 5,
     category: "REACT",
-    image: "/images/dog-main.jpg",
-    title: "Breed of Dogs",
-    description: `I built the Breed of Dogs App to showcase my knowledge and understanding of JavaScript (ES6+) and React.js in consuming REST API in developing useful software. The app features pagination, darkmode, and single-page rendering.I also used HTML5 & CSS Grid to make the app fully responsive.`,
-    github: "https://github.com/Danthesage01/dogs-app",
-    url: "https://dogsreact.netlify.app/",
-    technology: "HTML5, CSS3, ReactJS & React-Router-DOM"
+    image: "/images/quiz-main.jpg",
+    title: "Quiz App",
+    description: `I developed the Quiz App to showcase my knowledge and understanding of ReactJS in developing
+             interactive and responsive single-page applications. I used create react-app in building the App alongside several React Hooks such as the Context API, useEffect, and useState. I consumed a REST API that returns an array of random questions and answers which I massaged to create the quiz app. I got the project idea from one of Scrimba's solo projects.`,
+    github: "https://github.com/Danthesage01/quizzical",
+    url: "https://r-quizzical.netlify.app/",
+    technology: "HTML5, CSS3, ReactJS & Context API",
   },
   {
     id: 6,
@@ -60,7 +61,7 @@ const data = [
      CSS3 to develop modern, responsive and user-friendly websites, landing pages and web apps. The Shoe Vendor website was designed mainly with CSS Float and Animation. Also, I integrated some JavaScript concepts and functions to ensure the website fully dynamically and interactive.`,
     github: "https://github.com/Danthesage01/shoe-vendor",
     url: "https://shoe-vendor.netlify.app",
-    technology: "HTML5, CSS3, & JavaScript"
+    technology: "HTML5, CSS3, & JavaScript",
   },
   {
     id: 7,
@@ -70,7 +71,7 @@ const data = [
     description: `I built the color scheme app to showcase my knowledge and understanding of AJAX in JavaScript. I consumed a color schemed REST API in developing the project and massage the data to build a interactive and responsive user interface. I also used HTML5 & CSS flexbox to make the app fully responsive.`,
     github: "https://github.com/Danthesage01/color-scheme-project",
     url: "https://scheme-color.netlify.app",
-    technology: "HTML5, CSS3, & JavaScript"
+    technology: "HTML5, CSS3, & JavaScript",
   },
   {
     id: 8,
@@ -81,7 +82,7 @@ const data = [
             modern, responsive and user-friendly single-page applications. I used create react-app.`,
     github: "https://github.com/Danthesage01/shoes-reviews",
     url: "https://r-shoes-reviews.netlify.app/",
-    technology: "HTML5, CSS3, & ReactJS"
+    technology: "HTML5, CSS3, & ReactJS",
   },
   {
     id: 9,
@@ -91,7 +92,7 @@ const data = [
     description: `I built the Breed of Dogs App to showcase my knowledge and understanding of AJAX and JavaScript (ES6+) in consuming REST API in developing useful software. The app features pagination, darkmode, top scroll and single-page rendering.I also used HTML5 & CSS Grid to make the app fully responsive.`,
     github: "https://github.com/Danthesage01/dogAPI",
     url: "https://dogssapi.netlify.app/",
-    technology: "HTML5, CSS3, & JavaScript"
+    technology: "HTML5, CSS3, & JavaScript",
   },
   {
     id: 10,
@@ -102,8 +103,7 @@ const data = [
      CSS3 to develop modern, responsive and user-friendly websites, landing pages and web apps. The portfolio was designed mainly with CSS GRID. Also, I integrated some JavaScript concepts and functions to ensure the website fully dynamically and interactive.`,
     github: "https://github.com/Danthesage01/my-portfolio",
     url: "https://daniel-ayeni.netlify.app/",
-    technology: "HTML5, CSS3, & JavaScript"
-
+    technology: "HTML5, CSS3, & JavaScript",
   },
   {
     id: 11,
@@ -114,8 +114,18 @@ const data = [
             modern, responsive and user-friendly single-page applications. I used create react-app and utilised react-router-dom for easy navigations and routing within the app. Also, I created and hosted a REST API server on Heroku that serves as the backend where the guests'list is stored and fetched for CRUD operations.`,
     github: "https://github.com/Danthesage01/updated-react-guestapp",
     url: "https://guest-app.netlify.app/",
-    technology: "HTML5, CSS3, ReactJS & React-Router-DOM"
-  }
-]
+    technology: "HTML5, CSS3, ReactJS & React-Router-DOM",
+  },
+  {
+    id: 12,
+    category: "REACT",
+    image: "/images/dog-main.jpg",
+    title: "Breed of Dogs",
+    description: `I built the Breed of Dogs App to showcase my knowledge and understanding of JavaScript (ES6+) and React.js in consuming REST API in developing useful software. The app features pagination, darkmode, and single-page rendering.I also used HTML5 & CSS Grid to make the app fully responsive.`,
+    github: "https://github.com/Danthesage01/dogs-app",
+    url: "https://dogsreact.netlify.app/",
+    technology: "HTML5, CSS3, ReactJS & React-Router-DOM",
+  },
+];
 
 export default data
